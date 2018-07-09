925f107fcf00f97eb823979de84da2028230c676
zhchi-me <zhangchixx8@gmail.com>
2018-07-09 16:27:08 +0800

    swiper

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -1,7 +1,7 @@
 Page({
   onReady: function () {
-	this.draw;
-	this.interval = setInterval(this.draw, 17);
+	  this.draw
+	// this.interval = setInterval(this.draw, 17);
 	this.position = {
 		rad: 0,
 		add: Math.PI / 45
@@ -12,25 +12,44 @@ Page({
 	var p = this.position;
 	p.rad += p.add;
 
-	function nose(context, x, y, a, b, O, rad) {
-		var step = (a > b) ? 1 / a : 1 / b;
-		context.beginPath();
-		context.setStrokeStyle('#DE7EAE');
-		for (var i = 0; i < rad; i += step) {
-			context.lineTo(
-				x + a * Math.cos(i) * Math.cos(O) - b * Math.sin(i) * Math.sin(O),
-				y + a * Math.cos(i) * Math.sin(O) + b * Math.sin(i) * Math.cos(O)
-			);
-		}
-		context.stroke();
-	}
-	nose(context, 50, 100, 20, 30, 0.4, p.rad);
-	if (p.rad > 2 * Math.PI) {
-		clearInterval(this.interval);
-		context.setFillStyle('#F69FD2');
-		context.fill();
-		context.closePath();
-	}
+	// if (p.rad <= 2 * Math.PI) {
+	// 	nose(context, 50, 100, 20, 30, 0.4, p.rad);
+	// } else {
+	// 	nose(context, 50, 100, 20, 30, 0.4, Math.PI * 2);
+	// 	// clearInterval(this.interval);
+	// 	context.setFillStyle('#F69FD2');
+	// 	context.fill();
+	// 	context.closePath();
+	// 	nostril();
+	// }
+	// function nose(context, x, y, a, b, O, rad) {
+	// 	var step = (a > b) ? 1 / a : 1 / b;
+	// 	context.beginPath();
+	// 	context.setStrokeStyle('#DE7EAE');
+	// 	for (var i = 0; i < rad; i += step) {
+	// 		context.lineTo(
+	// 			//绘制椭圆，O为倾斜角度
+	// 			x + a * Math.cos(i) * Math.cos(O) - b * Math.sin(i) * Math.sin(O),
+	// 			y + a * Math.cos(i) * Math.sin(O) + b * Math.sin(i) * Math.cos(O)
+	// 		);
+	// 	}
+	// 	context.stroke();
+	// }
+	// function nostril () {
+	// 	context.beginPath();
+	// 	context.arc(40, 100, 5, 0, 2 * Math.PI);
+	// 	context.setFillStyle('#DE7EAE');
+	// 	context.fill();
+	// 	context.closePath();
+
+	// 	context.beginPath();
+	// 	context.arc(57, 102, 5, 0, 2 * Math.PI);
+	// 	context.fill();
+	// 	context.closePath();
+	// }
+
+
+
 
     wx.drawCanvas({
       canvasId: 'canvas',
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,46 +3,18 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World'
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  onShareAppMessage: function () {
-    return this.data.shareData
-  }
+	data: {
+		imgUrls: [
+			{ url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+			{ url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+			{ url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+			{ url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+		],
+
+		indicatorDots: false,
+		autoplay: false,
+		interval: 3000,
+		duration: 800,
+	}
+
 })
