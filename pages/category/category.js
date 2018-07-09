da071d660805fff9fb293cc80f979d3e17e44c17
zhchi-me <zhangchixx8@gmail.com>
2018-07-09 17:04:08 +0800

    分类

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,140 @@
 // pages/category/category.js
 Page({
+	data: {
+		category: [
+			{ name: '选项1', id: 'option1' },
+			{ name: '选项2', id: 'option2' },
+			{ name: '选项3', id: 'option3' }
+		],
+		detail: [
+			{
+				"id": "option1",
+				"banner": "/image/list1.png",
+				"cate": "option1",
+				"detail": [
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					}
+				]
+			},
+			{
+				"id": "option2",
+				"banner": "/image/list1.png",
+				"cate": "option2",
+				"detail": [
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					}
+				]
+			},
+			{
+				"id": "option3",
+				"banner": "/image/list1.png",
+				"cate": "option3",
+				"detail": [
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					},
+					{
+						"thumb": "/image/s1.png",
+						"name": "水果"
+					}
+				]
+			}
+		],
+		curIndex: 0,
+		isScroll: false,
+		toView: 'guowei'
+	},
+	
+	onReady() {
+		var self = this;
+		// wx.request({
+		// 	url: 'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+		// 	success(res) {
+		// 		self.setData({
+		// 			detail: res.data
+		// 		})
+		// 	}
+		// });
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+	switchTab(e) {
+		const self = this;
+		this.setData({
+			isScroll: true
+		})
+		setTimeout(function () {
+			self.setData({
+				toView: e.target.dataset.id,
+				curIndex: e.target.dataset.index
+			})
+		}, 0)
+		setTimeout(function () {
+			self.setData({
+				isScroll: false
+			})
+		}, 1)
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+	}
 })
