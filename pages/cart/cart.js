4c97edf963e9ee57930c5e17c673517eaf8c5bd9
zhchi-me <zhangchixx8@gmail.com>
2018-07-11 17:45:45 +0800

    cart

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,24 @@
-// pages/cart/cart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+	  goodsList: {
+		  list: []
+	  }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+ 
   onShow: function () {
-  
+	  var shopList = [0];
+	  var shopCarInfoMem = wx.getStorageSync('shopCarInfo');
+	  if (shopCarInfoMem) {
+		  shopList = shopCarInfoMem.shopNum
+	  }
+	  this.data.goodsList.list = shopList;
+	  this.setGoodsList(shopList);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  setGoodsList: function (list) {
+	  this.setData({
+		  goodsList: {
+			  list: list
+		  }
+	  });
   }
 })
