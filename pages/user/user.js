3bb810a48a6bb5e3f7444a8734847c333ac8c782
zhchi-me <zhangchixx8@gmail.com>
2018-07-12 10:58:18 +0800

    authorize login

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,9 +1,5 @@
-// pages/user/user.js
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
 	  thumb: '',
 	  nickname: '',
@@ -12,67 +8,35 @@ Page({
 	  address: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+
   onReady: function () {
   
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-	  var self = this;
-	  wx.getUserInfo({
-		  success: function (res) {
-			  self.setData({
-				  thumb: res.userInfo.avatarUrl,
-				  nickname: res.userInfo.nickName
-			  })
-		  }
+	  let that = this;
+	  let userInfo = wx.getStorageSync('userInfo')
+	  if (!userInfo) {
+		  wx.navigateTo({
+			  url: "/pages/authorize/authorize"
+		  })
+	  } else {
+		  that.setData({
+			  thumb: userInfo.avatarUrl,
+			  nickname: userInfo.nickName
+		  })
+	  }
+  },
+
+  aboutUs: function () {
+	  wx.showModal({
+		  title: '关于我们',
+		  content: '关于我们关于我们关于我们关于我们',
+		  showCancel: false
 	  })
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
